2968967f6b4def221121d3f733d9d468e9cbffa5
Car1smo <104145363+Car1smo@users.noreply.github.com>
2022-04-21 16:16:41 +0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,106 @@
+import ConfigParser
+import subprocess
+
+
+config = ConfigParser.RawConfigParser()
+
+config.read('k8s.config')
+client_count_kafka = (int(config.get('k8s.pods.number', 'kafka')))
+k8s_count_kafka = (int(subprocess.check_output("kubectl describe statefulset timeseries-kafka | grep 'Pods Status: .*$' | awk '{print $3}'", shell=True)))
+
+print('<__Kafka__>')
+print('')
+print('Client count:')
+print(client_count_kafka)
+print('K8s count:')
+print(k8s_count_kafka)
+print("")
+
+if client_count_kafka == k8s_count_kafka:
+    print('Compare result: Ok')
+elif client_count_kafka != k8s_count_kafka:
+    print('Compare result: Not ok')
+else:
+    print('Compare result: Error')
+
+print('----------------------------------')
+
+client_count_gateway = (int(config.get('k8s.pods.number', 'gateway')))
+k8s_count_gateway = (int(subprocess.check_output("kubectl describe deployment timeseries-gateway | grep 'Replicas: .*$' | awk '{print $2}'", shell=True)))
+
+print('<__Gateway__>')
+print('')
+print('Client count:')
+print(client_count_gateway)
+print('K8s count:')
+print(k8s_count_gateway)
+print("")
+
+if client_count_gateway == k8s_count_gateway:
+    print('Compare result: Ok')
+elif client_count_gateway != k8s_count_gateway:
+    print('Compare result: Not ok')
+else:
+    print('Compare result: Error')
+
+print('----------------------------------')
+
+client_count_pipeline = (int(config.get('k8s.pods.number', 'pipeline')))
+k8s_count_pipeline = (int(subprocess.check_output("kubectl describe deployment timeseries-pipeline | grep 'Replicas: .*$' | awk '{print $2}'", shell=True)))
+
+print('<__Pipeline__>')
+print('')
+print('Client count:')
+print(client_count_pipeline)
+print('K8s count:')
+print(k8s_count_pipeline)
+print("")
+
+if client_count_pipeline == k8s_count_pipeline:
+    print('Compare result: Ok')
+elif client_count_pipeline != k8s_count_gateway:
+     print('Compare result: Not ok')
+else:
+    print('Compare result: Error')
+
+print('----------------------------------')
+
+client_count_cassandra = (int(config.get('k8s.pods.number', 'cassandra')))
+k8s_count_cassandra = (int(subprocess.check_output("kubectl describe statefulset timeseries-kafka | grep 'Pods Status: .*$' | awk '{print $3}'", shell=True)))
+
+print('<__Cassandra__>')
+print('')
+print('Client count:')
+print(client_count_cassandra)
+print('K8s count:')
+print(k8s_count_cassandra)
+print("")
+
+if client_count_cassandra == k8s_count_cassandra:
+    print('Compare result: Ok')
+elif client_count_cassandra != k8s_count_cassandra:
+    print('Compare result: Not ok')
+else:
+    print('Compare result: Error')
+
+print('----------------------------------')
+
+client_count_query = (int(config.get('k8s.pods.number', 'query')))
+k8s_count_query = (int(subprocess.check_output("kubectl describe deployment timeseries-query | grep 'Replicas:.*$' | awk '{print $2}'", shell=True)))
+
+print('<__Query__>')
+print('')
+print('Client count:')
+print(client_count_query)
+print('K8s count:')
+print(k8s_count_query)
+print("")
+
+if client_count_query == k8s_count_query:
+    print('Compare result: Ok')
+elif client_count_query != k8s_count_query:
+    print('Compare result: Not ok')
+else:
+    print('Compare result: Error')
+
+print('----------------------------------')
